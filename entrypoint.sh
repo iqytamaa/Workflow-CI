631fe2317b21cea0b645780b3f9754a72c2cf0a8
iqytamaa <163631480+iqytamaa@users.noreply.github.com>
2025-10-07 23:17:59 +0700

    Fix: Add --no-conda flag to mlflow serve

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 RUN_ID=$(cat /app/run_id.txt)
 
 # Jalankan perintah mlflow serve dengan RUN_ID yang sudah dibaca
-exec conda run -n mlflow-env mlflow models serve -m /app/mlruns/0/$RUN_ID/artifacts/model -h 0.0.0.0 -p 8080
+exec conda run -n mlflow-env mlflow models serve -m /app/mlruns/0/$RUN_ID/artifacts/model -h 0.0.0.0 -p 8080 --no-conda
