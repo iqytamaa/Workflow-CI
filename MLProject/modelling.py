79222b906efa42a1cc20a1641e2ad40f7b0f6e0d
iqytamaa <163631480+iqytamaa@users.noreply.github.com>
2025-10-07 20:35:55 +0700

    Final fix for artifact pathing

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -16,7 +16,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 with mlflow.start_run() as run:
     run_id = run.info.run_id
     # Simpan run_id ke file untuk langkah Docker nanti
-    with open("run_id.txt", "w") as f:
+    with open("../run_id.txt", "w") as f:
         f.write(run_id)
 
     # Latih model
